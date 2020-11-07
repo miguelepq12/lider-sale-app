a1a89be3e242ec55a16b25ba33e9b3b2ea1214c6
Miguel Piña <miguelepq@gmail.com>
2020-11-07 03:07:55 -0300

    create components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchBarComponent } from './search-bar/search-bar.component';
+import { ProductsViewComponent } from './products-view/products-view.component';
+import { MainLayoutComponent } from './main-layout/main-layout.component';
+import { ProductListComponent } from './product/product-list/product-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchBarComponent,
+    ProductsViewComponent,
+    MainLayoutComponent,
+    ProductListComponent
   ],
   imports: [
     BrowserModule,
