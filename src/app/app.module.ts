f461287bff82bb832e6f4e2e0e573fa667edc9b3
Miguel Piña <miguelepq@gmail.com>
2020-11-11 03:52:51 -0300

    implement pagination ant

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,8 +17,9 @@ import {NzButtonModule} from 'ng-zorro-antd/button';
 import {NzInputModule} from 'ng-zorro-antd/input';
 import {NzIconModule} from 'ng-zorro-antd/icon';
 import {ProductService} from './products/shared/services/product.service';
-import {ProductProxyService} from "./products/shared/services/product-proxy.service";
+import {ProductProxyService} from './products/shared/services/product-proxy.service';
 import { PaginationComponent } from './views/pagination/pagination.component';
+import { NzPaginationModule } from 'ng-zorro-antd/pagination';
 
 registerLocaleData(es);
 
@@ -41,7 +42,8 @@ registerLocaleData(es);
     NzButtonModule,
     NzInputModule,
     NzIconModule,
-    HttpClientModule
+    HttpClientModule,
+    NzPaginationModule
   ],
   providers: [ProductService, ProductProxyService, { provide: NZ_I18N, useValue: es_ES }],
   bootstrap: [AppComponent]
